2c628ef118de00f9cb7b9b68f08fd9853deadc27
Eric Comanescu <ericcom@outlook.com>
2023-10-19 16:12:55 -0400

    forgot to include iostream

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 #include <vector>
 #include <utility.h>
@@ -5,9 +6,21 @@
 using namespace std;
 
 int part_1(const vector<string> data);
-int part_2(const vector<string> data);
+long part_2(const vector<string> data);
 
 int main()
 {
     vector<string> data = read_input("./input");
+
+    cout << "Part 1: " << part_1(data) << endl;
+    cout << "Part 2: " << part_2(data) << endl;
+}
+
+int part_1(const vector<string> data)
+{
+
+}
+long part_2(const vector<string> data)
+{
+
 }
